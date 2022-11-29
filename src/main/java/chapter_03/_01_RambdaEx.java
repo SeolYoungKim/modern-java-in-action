9f938ff658e5e767255192d870fe6d3774774f34
SeolYoungKim <nasur4da@gmail.com>
2022-11-29 22:45:59 +0900

    chapter03: 3.5강 형식 검사, 추론, 제약까지 정리

---
+++ src/main/java/chapter_03/_01_RambdaEx.java
@@ -0,0 +1,38 @@
+package chapter_03;
+
+import chapter_02.Apple;
+import java.nio.file.DirectoryStream.Filter;
+import java.util.Comparator;
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+
+public class _01_RambdaEx {
+
+    public static void main(String[] args) {
+        Function<String, Integer> stringToLength = (String s) -> s.length();
+        Filter<Apple> appleFilter = (Apple a) -> a.weight() > 150;
+//        (int x, int y) -> {
+//            System.out.println(x + y);
+//        };
+
+//        Comparator<Apple> appleComparator = (Apple a1, Apple a2) -> a1.weight() - a2.weight();
+        Comparator<Apple> appleComparator = Comparator.comparingInt(Apple::weight);
+
+        Runnable r1 = () -> System.out.println("HelloWorld!");
+        Runnable r2 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("HelloWorld!");
+            }
+        };
+
+        process(r1);
+        process(r2);
+        process(() -> System.out.println("HI!!!!!!!!!!!!!!!!!!"));
+
+    }
+
+    public static void process(Runnable r) {
+        r.run();
+    }
+}
